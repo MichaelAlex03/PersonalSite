b27c0fd2c3e4179bcfbb77e4a2c05fadc85b1153
Michael A Lleverino <michaelalleverino@Michaels-MacBook-Pro.local>
2025-02-09 12:54:40 -0600

    still working on projects section

---
+++ personalSite/src/components/projects/ProjectCard.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+interface CardProps {
+    name: string,
+    image: string,
+    description: string,
+    link?: string,
+    technologies: string[],
+    gitHub: string
+}
+
+const ProjectCard = ({ name, image, description, link, technologies, gitHub }: CardProps) => {
+    return (
+        <div className='flex flex-col rounded-md w-1/3 h-[500px] bg-[#26313F]'>
+
+            <div className='w-full'>
+                <img src={image} className='rounded-md'/>
+            </div>
+
+            <div className='flex flex-col items-center'>
+                <h1 className='font-bold text-2xl mt-2'>{name}</h1>
+                <p className='w-3/4 text-center text-[#A9A9A9] text-lg mt-4'>{description}</p>
+
+                <h4 className='mt-4'>Technologies Used</h4>
+                <div className='flex flex-row gap-4 mt-2'>
+                    {technologies.map(item => (
+                        <img src={`../../.././images/skills/${item}.svg`} height={20} width={20}/>
+                    ))}
+                </div>
+
+
+                <div className='flex flex-row gap-6 mt-4'>
+                    <button className='border border-white text-white hover:text-black hover:bg-[#55E5A4] py-2 px-4 rounded-lg'>
+                        <a href={gitHub} target="_blank">Source Code</a>
+                    </button>
+                    {link ? <button><a href={link} target='_blank'>Site</a></button> : null}
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ProjectCard
--- personalSite/src/pages/projects.tsx
+++ personalSite/src/pages/projects.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import ProjectCard from "../components/projects/ProjectCard";
 
 const Projects = () => {
 
@@ -51,79 +52,37 @@ const Projects = () => {
       </div>
 
       {/*Projects*/}
-      <div className='flex flex-col items-center justify-between space-y-10 md:space-y-16'>
+      <div className='flex flex-row items-center justify-center space-x-8'>
 
         {/*NASA Project*/}
-        <div className='flex flex-col items-center gap-4'>
 
-          <div className='flex object-cover w-4/5 xl:w-3/5'>
-            <img src='../images/projectImages/NASA.png' alt='Apartment project reviews page' className='shadow-2xl w-full rounded-3xl border-4 border-black' />
-          </div>
-
-          <div className='flex flex-col items-center w-3/4 md:w-3/5 lg:w-1/2 space-y-4'>
-
-            <div className='text-center mb-1'>
-              <h2 className='font-bold md:font-extrabold text-xl md:text-3xl lg:text-4xl'>Nasa Control Center Web App</h2>
-              <p className='flex flex-col text-sm md:text-lg lg:text-xl'>Full stack NASA control center using React, Node.js/Express.js, and MongoDB</p>
-            </div>
-
-            <div className='z-10 w-full flex justify-center'>
-              <button className='button_sub border-2 border-white text-sm md:text-base lg:text-lg md:p-1 w-2/3 md:w-1/3'>
-                <a href="https://github.com/MichaelAlex03/NasaAPI" target="_blank">Source Code</a>
-              </button>
-            </div>
-
-          </div>
-
-        </div>
-
+        <ProjectCard
+          name="Nasa Control Center"
+          image="../images/projectImages/NASA.png"
+          description="A simulated mission control dashboard for launching and managing space missions with real-time data visualization"
+          gitHub="https://github.com/MichaelAlex03/NasaAPI"
+          technologies={['react', 'nodejs', 'mongodb', 'docker', 'aws']}
+        />
+        
         {/*FitTrackr*/}
-        <div className='flex flex-col items-center gap-4'>
 
-          <div className='flex w-4/5 xl:w-3/5'>
-            <img src='../images/projectImages/workout.png' alt='FitTrack workout page' className=' shadow-2xl w-full rounded-3xl border-4 border-black' />
-          </div>
-
-          <div className='flex flex-col items-center w-3/4 md:w-3/5 lg:w-1/2 space-y-4'>
-
-            <div className='text-center mb-1'>
-              <h2 className='font-bold md:font-extrabold text-xl md:text-3xl lg:text-4xl'>FitTrackr Web App</h2>
-              <p className='flex flex-col text-sm md:text-lg lg:text-xl'>Full stack workout tracker site using React, Node.js/Express.js, and PostgreSQL</p>
-            </div>
-
-            <div className='z-10 w-full flex justify-center'>
-              <button className='button_sub text-sm md:text-base lg:text-lg md:p-1 w-2/3 md:w-1/3'>
-                <a href="https://github.com/MichaelAlex03/FitTrackr" target="_blank">Source Code</a>
-              </button>
-            </div>
-
-          </div>
-
-        </div>
+        <ProjectCard
+          name="FitTrackr"
+          image="../images/projectImages/workout.png"
+          description="Full stack workout tracker site using React, Node.js/Express.js, and PostgreSQL"
+          gitHub="https://github.com/MichaelAlex03/FitTrackr"
+          technologies={['react', 'tailwindcss', 'nodejs', 'postgresql']}
+        />
 
         {/*Tank Game*/}
-        <div className='flex flex-col items-center gap-4'>
 
-          <div className='flex w-4/5 xl:w-3/5'>
-            <img src='../images/projectImages/TankGame.png' alt='Tank game top down view' className=' shadow-xl w-full rounded-3xl border-4 border-black' />
-          </div>
-
-          <div className='flex flex-col items-center w-3/4 md:w-3/5 lg:w-1/2 space-y-4'>
-
-            <div className='text-center mb-1'>
-              <h2 className='font-bold md:font-extrabold text-xl md:text-3xl lg:text-4xl'>Top Down Tank Game</h2>
-              <p className='flex flex-col text-sm md:text-lg lg:text-xl'>Top down tank game built with React, Javascript, Node.js/Express.js, and MongoDB</p>
-            </div>
-
-            <div className='z-10 w-full flex justify-center'>
-              <button className='button_sub border-2 border-white text-sm md:text-base lg:text-lg md:p-1 w-2/3 md:w-1/3'>
-                <a href="https://github.com/MichaelAlex03/CS_4398" target="_blank">Source Code</a>
-              </button>
-            </div>
-
-          </div>
-
-        </div>
+        <ProjectCard
+          name="Top Down Tank Game"
+          image="../images/projectImages/TankGame.png"
+          description="Top down tank game built with React, Javascript, Node.js/Express.js, and MongoDB"
+          gitHub="https://github.com/MichaelAlex03/CS_4398"
+          technologies={['javascript', 'tailwindcss', 'nodejs', 'mongodb']}
+        />
 
       </div>
 
