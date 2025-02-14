c66688a018e0dbb4d3fb31f910199d04d2e49236
Your Name <michaelalex03@outlook.com>
2025-02-14 15:03:13 -0600

    changed unselected opacity back to 0

--- personalSite/src/components/Navbar.tsx
+++ personalSite/src/components/Navbar.tsx
@@ -75,7 +75,7 @@ const Navbar = ({ currentPage, setCurrentPage }: NavProps) => {
 
                     
                     <div className={`lg:hidden flex absolute top-16 left-0 w-full h-auto bg-[#222831] flex-col items-center gap-6 font-semibold z-50 p-4 transition-all duration-300 ease-in-out transform 
-                    ${showMenu ? "opacity-100 translate-y-0" : "opacity-50 -translate-y-40"}`}
+                    ${showMenu ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-40"}`}
                     >
                         <MobileViewLinks currentPage={currentPage} setCurrentPage={setCurrentPage}/>
                     </div>
