c25e43f5d6d2bafe4077cecd5cabbdf4a16fa201
Your Name <michaelalex03@outlook.com>
2025-02-16 16:24:26 -0600

    code no deploy

--- personalSite/src/components/projects/ProjectCard.tsx
+++ personalSite/src/components/projects/ProjectCard.tsx
@@ -5,8 +5,10 @@ interface CardProps {
     link?: string,
     technologies: string[],
     gitHub: string
+
 }
 
+{/*test*/}
 const ProjectCard = ({ name, image, description, link, technologies, gitHub}: CardProps) => {
     return (
         <div className='flex flex-col rounded-md w-full bg-[#26313F] transition duration-300 ease-in-out transform hover:scale-105'>
