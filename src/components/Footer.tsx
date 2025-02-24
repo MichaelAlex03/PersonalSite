fe60ec67869067f223d310cbda7492044fc4449c
MichaelAlex03 <michaelalex03@outlook.com>
2025-02-24 02:16:54 -0600

    added smooth transition for footer elements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,21 +3,22 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
+
   return (
     <footer className='absolute bottom-0 bg-[#222831] flex flex-col items-center justify-center p-8 drop-shadow-md w-full gap-2'>
 
       <div className='flex gap-2 '>
-        <a href="https://www.linkedin.com/in/michaelllev/" target="_blank" className='hover:scale-110'>
+        <a href="https://www.linkedin.com/in/michaelllev/" target="_blank" className='hover:scale-110 transition duration-300 '>
           <FontAwesomeIcon icon={faLinkedin} 
           size='2xl'
-          color='white'/>
+          color='white' />
         </a>
-        <a href="https://github.com/MichaelAlex03" target="_blank" className='hover:scale-110'>
+        <a href="https://github.com/MichaelAlex03" target="_blank" className='hover:scale-110 transition duration-300'>
           <FontAwesomeIcon icon={faGithub} 
             size='2xl'
             color='white'/>
         </a>
-        <a href="mailto:michaelalex03@outlook.com" target="_blank" className='hover:scale-110'>
+        <a href="mailto:michaelalex03@outlook.com" target="_blank" className='hover:scale-110 transition duration-300'>
           <FontAwesomeIcon icon={faEnvelope} 
             size='2xl'
             color='white'/>
